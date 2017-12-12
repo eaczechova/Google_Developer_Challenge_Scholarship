dab19328e33119f902905a1c18747a147da481ea
Ewa Czech <eaprojekty@gmail.com>
2017-12-12 21:14:52 +0100

    initial commit

---
+++ 2_12_one_awesome_message.js
@@ -0,0 +1,26 @@
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables:
+ *     - firstName
+ *     - interest
+ *     - hobby
+ * 2. Create a variable named awesomeMessage and, using string concatenation
+      and the variables above, create an awesome message.
+ * 3. Print the awesomeMessage variable to the console.
+ */
+
+/*
+ * Notes:
+ * - Using the above as an example, firstName would have been assigned to
+ *   "Julia", interest to "cats", and hobby to "to play video games".
+ * - Be sure to include spaces and periods where necessary!
+ */
+
+// Add your code here
+
+var firstName = "Ewa";
+var interest = "coding";
+var hobby = "go running";
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+console.log(awesomeMessage);
